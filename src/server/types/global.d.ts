92cc432d1ebc976eb4d2d328741faed69da30ac2
Sitt Guruvanich <aekazitt@gmail.com>
2023-05-29 12:19:06 +0700

    Add secret attributes to process interface

--- src/server/types/global.d.ts
+++ src/server/types/global.d.ts
@@ -3,6 +3,8 @@ declare module 'process' {
     namespace NodeJS {
       interface ProcessEnv {
         readonly BTC_RPC_URL: string
+        readonly COOKIE_SECRET: string
+        readonly CSRF_SECRET: string
         readonly LND_MACAROON_PATH: string
         readonly LND_TLSCERT_PATH: string
         readonly NODE_ENV?: 'development' | 'production'
