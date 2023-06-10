c4aef1869a7231b1642efc6eca5fd6d81da851e7
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-10 14:16:04 +0700

    Remove unsed attribute

--- tests/configs.ts
+++ tests/configs.ts
@@ -4,7 +4,6 @@ import 'dotenv/config'
 export const lnd = {
   host: process.env.LND_SERVICE_HOST || 'localhost',
   macaroonPath: process.env.LND_MACAROON_PATH || './admin.macaroon',
-  password: '',
   port: parseInt(process.env.LND_SERVICE_PORT || '10009'),
   protoPath: './public/rpc.proto',
   tlsCertPath: process.env.LND_TLSCERT_PATH || './tls.cert',
