5076b4e51d1d548a65fc62d27674b2b8b3fd0175
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-12 09:43:26 +0700

    Define Tag type with BOLT11 TagData

--- src/types/Tag.ts
+++ src/types/Tag.ts
@@ -1,9 +1,12 @@
 // ~~/src/types/Tag.ts
 
+// imports
+import type { TagData } from 'bolt11'
+
 // define type
 export type Tag = {
   tagName: string
-  data: string
+  data: TagData
 }
 
 export default Tag
