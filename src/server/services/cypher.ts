956ea96031e0fe2f478a0981ac2c1bf4f2e8ddec
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-15 16:02:53 +0700

    Shorten code

--- src/server/services/cypher.ts
+++ src/server/services/cypher.ts
@@ -1,36 +1,59 @@
 // ~~/src/server/services/cypher.ts
 
 // imports
-import bitcoin, { Transaction } from 'bitcoinjs-lib'
+import { Network, Transaction, address, networks, script } from 'bitcoinjs-lib'
 import classify from 'bitcoinjs-lib/src/classify'
 
-const decodeFormat = (tx: Transaction) => ({
-  txid: tx.getId(),
-  version: tx.version,
-  locktime: tx.locktime,
-})
+// types
+type Input = {
+  n: number
+  script: string
+  sequence: number
+  txid: string
+}
 
-const decodeInput = (tx: Transaction) => {
-  const result = []
-  tx.ins.forEach((input, n) => {
+type Output = {
+  satoshi: number
+  value: number
+  n: number
+  scriptPubKey: {
+    asm: string
+    hex: string
+    type: string
+    addresses: string[]
+  }
+}
+
+let decodeFormat = (transaction: Transaction): {
+  locktime: number;
+  txid: string;
+  version: number } => ({
+    txid: transaction.getId(),
+    version: transaction.version,
+    locktime: transaction.locktime,
+  })
+
+let decodeInput = (transaction: Transaction): Array<Input> => {
+  let result: Input[] = []
+  transaction.ins.forEach((input, n) => {
     result.push({
       txid: input.hash.reverse().toString('hex'),
       n: input.index,
-      script: bitcoin.script.toASM(input.script),
+      script: script.toASM(input.script),
       sequence: input.sequence,
     })
   })
   return result
 }
 
-const decodeOutput = (tx: Transaction, network) => {
-  const format = (out, n, network) => {
-    const vout = {
+let decodeOutput = (network: Network, transaction: Transaction): Array<Output> => {
+  let format = (out, n, network) => {
+    let vout = {
       satoshi: out.value,
       value: (1e-8 * out.value).toFixed(8),
       n: n,
       scriptPubKey: {
-        asm: bitcoin.script.toASM(out.script),
+        asm: script.toASM(out.script),
         hex: out.script.toString('hex'),
         type: classify.output(out.script),
         addresses: [],
@@ -39,53 +62,32 @@ const decodeOutput = (tx: Transaction, network) => {
     switch (vout.scriptPubKey.type) {
       case 'pubkeyhash':
       case 'scripthash':
-        vout.scriptPubKey.addresses.push(bitcoin.address.fromOutputScript(out.script, network))
+        vout.scriptPubKey.addresses.push(address.fromOutputScript(out.script, network))
         break
       case 'witnesspubkeyhash':
       case 'witnessscripthash':
-        const data = bitcoin.script.decompile(out.script)[1]
+        const data = script.decompile(out.script)[1]
         // @ts-ignore
-        vout.scriptPubKey.addresses.push(bitcoin.address.toBech32(data, 0, network.bech32))
+        vout.scriptPubKey.addresses.push(address.toBech32(data, 0, network.bech32))
         break
     }
     return vout
   }
-
-  const result = []
-  tx.outs.forEach((out, n) => {
+  let result = []
+  transaction.outs.forEach((out, n) => {
     result.push(format(out, n, network))
   })
   return result
 }
 
-class TxDecoder {
-  tx: Transaction
-  format: { locktime: number; txid: string; version: number }
-  constructor(rawTx, network = bitcoin.networks.bitcoin) {
-    this.tx = bitcoin.Transaction.fromHex(rawTx)
-    this.format = decodeFormat(this.tx)
-    // @ts-ignore
-    this.inputs = decodeInput(this.tx)
-    // @ts-ignore
-    this.outputs = decodeOutput(this.tx, network)
-  }
-
-  decode() {
-    const result = {}
-    const self = this
-    Object.keys(self.format).forEach(key => {
-      result[key] = self.format[key]
-    })
-    // @ts-ignore
-    result.outputs = self.outputs
-    // @ts-ignore
-    result.inputs = self.inputs
-    return result
-  }
-}
-
-export const decodeRawHex = (rawTx, network = bitcoin.networks.bitcoin) => {
-  return new TxDecoder(rawTx, network).decode()
+export const decodeRawHex = (rawTransaction: string) => {
+  let transaction = Transaction.fromHex(rawTransaction)
+  let format = decodeFormat(transaction)
+  let result = {}
+  Object.keys(format).forEach(key => result[key] = format[key])
+  let inputs: Input[] = decodeInput(transaction)
+  let outputs: Output[] = decodeOutput(networks.bitcoin, transaction)
+  return { inputs, outputs, ...result }
 }
 
 export default decodeRawHex
