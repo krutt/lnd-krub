e0df9fb65e588df9f0a4829654e8a00b256406f5
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-16 13:57:03 +0700

    Validate types returned

--- tests/lndhub/public.spec.ts
+++ tests/lndhub/public.spec.ts
@@ -28,10 +28,12 @@ describe('GET /getinfo', () => {
         (response: { body: { uris: string[]; chains: { chain: string; network: string }[] } }) => {
           let { chains, uris } = response.body
           expect(chains).toBeTruthy()
+          expect(chains).toBeTypeOf('object') // array
           expect(chains.length).toBe(1)
           expect(chains[0].chain).toBe('bitcoin')
           expect(chains[0].network).toBe('regtest')
           expect(uris).toBeTruthy()
+          expect(uris).toBeTypeOf('object') // array
           expect(uris.length).toBe(1)
         }
       )
