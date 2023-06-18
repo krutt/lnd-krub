76ed75a5f1697f350c79f2d4b7269f8817bb99ba
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-18 14:29:38 +0700

    Improve typing and docs for graph store

--- src/server/stores/graph.ts
+++ src/server/stores/graph.ts
@@ -5,17 +5,28 @@ import type { Graph } from '@/types'
 import { cache, lightning } from '@/server/stores'
 import { promisify } from 'node:util'
 
+// constants
+const KEY: string = 'lightning_describe_graph'
+const TTL: number = 120000
+
+/**
+ * Asks the LND instance to describe graphical connections to other lightning nodes on the network
+ * Fetches the graph description either from LRU cached response or fresh from the daemon
+ * @returns {Graph}
+ */
 export const describeLightningGraph = async (): Promise<Graph> => {
-  let graph = JSON.parse(await cache.get('lightning_describe_graph'))
+  let graph = JSON.parse(await cache.get(KEY))
   if (!graph) {
     let graph: { edges: any } = await promisify(lightning.describeGraph)
       .bind(lightning)({ include_unannounced: true })
       .catch(console.error)
-    if (graph) await cache.setex('lightning_describe_graph', 120000, JSON.stringify(graph))
+    if (graph) await cache.setex(KEY, TTL, JSON.stringify(graph))
   }
   return graph
 }
 
-export const releaseLightningGraph = async (): Promise<void> => {
-  await cache.del('lightning_describe_graph')
-}
+/**
+ * Unsets the cached lightning graph from LND instance
+ * @returns {Number}
+ */
+export const releaseLightningGraph = async (): Promise<number> => await cache.del(KEY)
