77aa70d962c57c4b3f6bf0f54f3200bc7bb4ba98
Sitt Guruvanich <aekazitt@gmail.com>
2023-06-20 14:47:56 +0700

    Refactor default macaroon and tlscert filenames to extenral

--- tests/configs.ts
+++ tests/configs.ts
@@ -2,10 +2,10 @@
 
 export const externalLND = {
   host: process.env.LND_TARGET_HOST || 'localhost',
-  macaroonPath: process.env.LND_TARGET_MACAROON_PATH || './target.macaroon',
+  macaroonPath: process.env.LND_TARGET_MACAROON_PATH || './external.macaroon',
   port: parseInt(process.env.LND_TARGET_PORT || '10010'),
   protoPath: './public/rpc.proto',
-  tlsCertPath: process.env.LND_TARGET_TLSCERT_PATH || './target.cert',
+  tlsCertPath: process.env.LND_TARGET_TLSCERT_PATH || './external.cert',
 }
 
 export default { externalLND }
