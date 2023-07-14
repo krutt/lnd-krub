0b5069831f25a927fbd3370ebb89ad81fefc64d6
Sitt Guruvanich <aekazitt@gmail.com>
2023-07-14 14:06:25 +0700

    Integrate @prisma/client in stores

--- src/server/stores/index.ts
+++ src/server/stores/index.ts
@@ -4,8 +4,10 @@
 import { BitcoinService } from '@/server/services/bitcoin'
 import { CacheService } from '@/server/services/cache'
 import { LightningService } from '@/server/services/lightning'
+import { PrismaClient } from '@prisma/client'
 
 // exports
 export const bitcoin = new BitcoinService()
 export const cache = new CacheService()
 export const lightning = new LightningService()
+export const prisma = new PrismaClient()
