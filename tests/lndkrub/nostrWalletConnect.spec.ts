7902c08fa68a3905d1fbcad02513030a38468f6f
Sitt Guruvanich <aekazitt@gmail.com>
2023-12-08 21:05:38 +0700

    Attempt to publish nostr+walletconnect string

--- tests/lndkrub/nostrWalletConnect.spec.ts
+++ tests/lndkrub/nostrWalletConnect.spec.ts
@@ -1,10 +1,13 @@
 /* ~~/tests/lndkrub/info.spec.ts */
 
 // imports
+// import 'websocket-polyfill'
+import { SimplePool } from 'nostr-tools'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { bytesToHex } from '@noble/hashes/utils'
 import lndkrub from '@/index'
 import { schnorr } from '@noble/curves/secp256k1'
+import { sha256 } from '@noble/hashes/sha256'
 import supertest from 'supertest'
 
 let privkey: string
@@ -27,7 +30,7 @@ describe('POST /nwc', () => {
       .send({ pubkey })
       .expect(200)
       .expect('Content-Type', /application\/json/)
-      .then((response: { body: { connection: string } }) => {
+      .then(async (response: { body: { connection: string } }) => {
         let { connection } = response.body
         let url: URL = new URL(decodeURIComponent(connection))
         expect(url).toBeTruthy()
@@ -39,6 +42,23 @@ describe('POST /nwc', () => {
         let secret = url.searchParams.get('secret')
         expect(secret).toBeTruthy()
         expect(secret.length).toBe(44)
+
+        let event = {
+          id: null,
+          sig: null,
+          pubkey,
+          created_at: Math.round(Date.now() / 1000),
+          content: 'test',
+          kind: 22345,
+          tags: [],
+        }
+        event.pubkey = pubkey
+        let utf8Encoder = new TextEncoder()
+        event.id = bytesToHex(sha256(utf8Encoder.encode(JSON.stringify([0, event.pubkey, event.created_at, event.kind, event.tags, event.content]))))
+        event.sig = bytesToHex(schnorr.sign(event.id, privkey))
+        let pool: SimplePool = new SimplePool()
+        await pool.publish([relay], event)
+        pool.close([relay])
       })
   })
 })
