2f0d08fb70535ec6c836a9d193231d40803c14ce
Sitt Guruvanich <aekazitt@gmail.com>
2024-01-04 13:29:44 +0700

    Add creation_date and state attributres to Invoice

--- src/types/Invoice.ts
+++ src/types/Invoice.ts
@@ -12,6 +12,7 @@ export type AddInvoiceResponse = {
 export type Invoice = {
   add_index: string
   amt: number // amount
+  creation_date: string // unix-timestamp
   description: string // memo
   destination: string
   expiry: number
@@ -22,9 +23,37 @@ export type Invoice = {
   payment_hash: string
   payment_request: string
   r_hash: Buffer
+  state: 'OPEN' | 'SETTLED'
   timestamp?: number // unix timestamp in seconds
   type: 'faucet' | 'user_invoice'
 }
+
+// export type InvoiceExtended = {
+//   route_hints: string[],
+//   htlcs: string[],
+//   features: Object[],
+//   memo: string,
+//   r_preimage: Buffer
+//   r_hash: Buffer
+//   value: string
+//   settled: boolean
+//   creation_date: string
+//   settle_date: string
+//   payment_request: string // bolt11
+//   description_hash: Buffer
+//   expiry: string
+//   fallback_addr: string
+//   cltv_expiry: string
+//   private: boolean
+//   add_index: string
+//   settle_index: string
+//   amt_paid: string
+//   amt_paid_sat: string  // number-like
+//   amt_paid_msat: string // number-like
+//   state: 'OPEN' | 'SETTLED'
+//   value_msat: string // number-like
+//   is_keysend: boolean
+// }
 
 export type InvoiceJSON = Invoice & {
   r_hash: {
