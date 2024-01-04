7d8eade1ef39b11dc24d1291b6087c8751e67b5d
Sitt Guruvanich <aekazitt@gmail.com>
2024-01-04 13:47:30 +0700

    Define job to handle unpaid invoices

--- jobs/unpaid_invoices.ts
+++ jobs/unpaid_invoices.ts
@@ -1,33 +1,19 @@
+/* ~/jobs/unpaid_invoices.ts */
+
+// imports
+import { listInvoices, markAsPaidInDatabase } from '@/server/stores/invoice'
+
 /**
- * This script goes through all user invoices in LND and if it is settled - marks it
- * so in our database. Does this only for invoices younger than week. *
+ * Go through all user invoices in LND and checks if it has been settled, mark it as
+ * such on the database if its creation time is maximally with two weeks prior.
  */
-import { createInvoice, savePreimage } from '@/server/stores/invoice'
-// const config = require('../config')
-
-(async () => {
-  console.log('unpaid invoices')
-
-  // console.log('fetching listinvoices...')
-  // let tempInv = new Invo(redis, bitcoinclient, lightning)
-
-  // let listinvoices = await tempInv.listInvoices()
-  // console.log('done', 'got', listinvoices['invoices'].length, 'invoices')
-  // fs.writeFileSync('listInvoices.json', '[\n')
-
-  // let markedInvoices = 0
-  // for (const invoice of listinvoices['invoices']) {
-  //   fs.appendFileSync('listInvoices.json', JSON.stringify(invoice, null, 2) + ',\n')
-  //   if (invoice.state === 'SETTLED' && +invoice.creation_date >= +new Date() / 1000 - 3600 * 24 * 7 * 2) {
-  //     tempInv.setInvoice(invoice.payment_request)
-  //     await tempInv.markAsPaidInDatabase()
-  //     markedInvoices++
-  //     process.stdout.write(markedInvoices + '\r')
-  //   }
-  // }
-
-  // fs.appendFileSync('listInvoices.json', ']')
-
-  // console.log('done, marked', markedInvoices, 'invoices')
+;(async () => {
+  await listInvoices().then(invoices => {
+    invoices['invoices'].forEach(async invoice => {
+      invoice.state == 'SETTLED' && +invoice.creation_date >= +new Date() / 1000 - 3600 * 24 * 7 * 2
+        ? await markAsPaidInDatabase(invoice.payment_request)
+        : void 0
+    })
+  })
   process.exit()
 })()
